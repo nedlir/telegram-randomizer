0c931c9429244d4b180e54ba698042fef87ecc2c
nedlir <neliran2@telem.openu.ac.il>
2020-08-24 08:11:54 +0200

    added example

---
+++ example.py
@@ -0,0 +1,16 @@
+import os
+
+from dotenv import load_dotenv
+
+import randomizer
+
+
+load_dotenv()
+API_HASH = os.getenv('TELEGRAM_API_HASH')
+API_ID = os.getenv('TELEGRAM_API_ID')
+WORKING_DIRECTORY = os.getenv('WORKING_DIRECTORY')
+PHONE = os.getenv('PHONE')
+
+os.chdir(WORKING_DIRECTORY)
+
+randomizer.change_data(API_ID, API_HASH)
